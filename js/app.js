501090d7d3ffa3cf4983d549ae6beac917a82ebc
Aaron Dweck <aarondweck24@gmail.com>
2024-12-15 15:22:07 +0000

    Added characters to grid

--- js/app.js
+++ js/app.js
@@ -102,6 +102,40 @@ const columns = 15
 const numOfCells = rows * columns
 const cells = []
 const indxOfOpenCells = [16, 17, 18, 19, 20, 21, 23, 24, 25, 26, 27, 28, 31, 33, 36, 38, 41, 41, 43, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 61, 63, 65, 69, 71, 73, 76, 78, 80, 81, 82, 83, 84, 86, 88, 91, 93, 97, 101, 103, 106, 107, 108, 110, 111, 112, 113, 114, 116, 117, 118, 123, 125, 129, 131, 135, 136, 137, 138, 139, 140, 144, 145, 146, 147, 148, 149, 153, 155, 156, 157, 158, 159, 161, 166, 167, 168, 169, 175, 176, 177, 178, 181, 184, 185, 186, 187, 188, 189, 190, 193, 196, 197, 199, 205, 207, 208, 212, 214, 215, 216, 218, 219, 220, 222, 226, 227, 228, 229, 231, 232, 233, 235, 236, 237, 238, 241, 247, 253, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268,]
+const characters = {
+    pacman: {
+        startIndex: 187,
+        currentIndex: 0,
+    },
+    ghosts: {
+        red: {
+            name: 'red',
+            startIndex: 127,
+            currentIndex: 0,
+        },
+        blue: {
+            name: 'blue',
+            startIndex: 141,
+            currentIndex: 0,
+        },
+        pink: {
+            name: 'pink',
+            startIndex: 142,
+            currentIndex: 0,
+        },
+        orange: {
+            name: 'orange',
+            startIndex: 143,
+            currentIndex: 0,
+        },
+    }
+}
+const pacman = characters.pacman
+const redGhost = characters.ghosts.red
+const blueGhost = characters.ghosts.blue
+const pinkGhost = characters.ghosts.pink
+const orangeGhost = characters.ghosts.orange
+const arrOfGhosts = [redGhost, blueGhost, pinkGhost, orangeGhost]
 
 /*------------------------cached elements----------------------*/
 const scoreEl = document.querySelector('#score')
@@ -123,4 +157,22 @@ for (let index = 0; index < numOfCells; index++) {
 
 }
 
+function removeCharaters(){
+    cells[pacman.startIndex].classList.remove('pacman')
+    for (const ghost of arrOfGhosts) {
+        cells[ghost.startIndex].classList.remove('ghost', ghost.name)
+    }
+}
+
+function setCharactersToStart (){
+    removeCharaters()
+    // adding the characters to the start
+    cells[pacman.startIndex].classList.add('pacman')
+    for (const ghost of arrOfGhosts) {
+        cells[ghost.startIndex].classList.add('ghost', ghost.name)
+    }
+
+}
+
+setCharactersToStart()
 
