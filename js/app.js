4dcc3fc82995ee37550844cfcaad43db16f07b2c
Aaron Dweck <aarondweck24@gmail.com>
2026-02-09 17:53:07 +0200

    added interval array and renamed some ghost variables

--- js/app.js
+++ js/app.js
@@ -1,89 +1,3 @@
-/*------------------------JS----------------------*/
-
-// ! mvp only need start button
-// * when the page opens the user will have a start button in the center with the background 
-// * of the board blured out when the user presses start the button and blur will clear out and start the game
-
-// board setup
-// have a list of the index of each wall or open space 
-// create a list of each cell in the grid
-// add x and y value to each cell // ! not needed for mvp
-// set the positions of the characters on the board
-// set the current positon of each character to their start index
-// add the class of a character to the correct index
-// if it is the start of a new round set each correct open space to have a dot in it
-// add 1 to the number of dots
-
-//when start button is pressed
-// start the intro music
-// when music is done start the game with the pacman moving with a defualt left direction
-
-// pacman's moves
-// create an interval with the timing of its speed corresponding to the game speed
-// using a parameter of the current direction and preferred direction
-// if the preferred direction is a legal place to move
-// move to that space and set the current direction
-// otherwise
-// if the current direction is a legal place to move
-// move
-// otherwise
-// stop 
-
-// the ghost's moves
-// create an interval with the timing of its speed corresponding to the game speed
-// look ahead
-// look at next space with current direction
-// get legal spaces around it (its previous position is not legal)
-// ! mvp is just to make it move by its self which can be made by choosing first available spot
-// if length of possible moves is equal to one 
-// move to that position
-// otherwise
-// look at each available spot and calculate distance to target cell
-// return the cell with the shortest distance
-// move into space with current direction
-// set direction to move in the cell with shortest distance
-
-// moving a character
-// get character and direction from parameter
-
-// remove the character's class from current cell
-
-// if the character direction is left
-// if the character is in the left tunnel
-// set the characters position to the right tunnel
-// otherwise
-// add the chracters class to the cell with one less index
-
-// if the character direction is right 
-// if the character is in the right tunnel
-// set the characters position to the left tunnel
-// otherwise
-// add the chracters class to the cell with one more index
-
-// if the character direction is up 
-// add the chracters class to the cell with  an index thats a columns length lesser
-
-// if the character direction is down 
-// add the chracters class to the cell with  an index thats a columns length greater
-
-// check pacmans cell
-// if cell has a dot in it
-// play eating sound
-// clear the dot and add 10 points to the score and 1 to collected dots
-// if collected dots are equal to the number of dots
-// start new round
-// if cell has ghost in it
-// if afraid mode is on
-// set that objects class to return and set its target to home
-// otherwise
-// play dying sound
-// minus one from the life
-// if lives are equal to zero
-// end game and display score
-// reset positions
-// start game 
-
-
 /*------------------------constants----------------------*/
 
 const rows = 19
@@ -158,6 +72,7 @@ const arrOfGhosts = [redGhost, blueGhost, pinkGhost, orangeGhost]
 const ghostRestartIndex = 112
 const gameSpeed = 300
 const livesArr = []
+const arrOfIntervals = []
 
 /*------------------------cached elements----------------------*/
 
@@ -323,8 +238,7 @@ function getShortestDistance(arrayOfIdexes) {
 }
 
 function ghostsMoves() {
-    // the ghost's moves
-    // create an interval with the timing of its speed corresponding to the game speed
+    // creating an interval with the timing of its speed corresponding to the game speed
     ghostInterval = setInterval(() => {
         arrOfGhosts.forEach(ghost => {
             if (ghost.locked) {
@@ -334,29 +248,25 @@ function ghostsMoves() {
                 const la = getNextIndex(ghost.currentIndex, ghost.currentDirection)
                 const directions = ['up', 'down', 'left', 'right']
                 // get legal spaces around it (its previous position is not legal)
-                const legalIndexes = directions.map(direction => {
+                const allIndexes = directions.map(direction => {
                     return getNextIndex(la, direction)
                 })
-                const filteredIndexes = legalIndexes.filter(index => {
+                const legalIndexes = allIndexes.filter(index => {
                     if (index === ghost.currentIndex) {
                         return false
                     } else {
                         return checkCellLegality(index)
                     }
                 })
-                // ! mvp is just to make it move by its self which can be made by choosing first available spot
-                // if length of possible moves is equal to one
-                if (filteredIndexes.length === 1) {
-                    ghost.currentDirection = getDirection(filteredIndexes[0], la)
+                if (legalIndexes.length === 1) {
+                    ghost.currentDirection = getDirection(legalIndexes[0], la)
                 } else {
-                    // otherwise
                     if (ghost.frightened) {
-                        const randomCell = filteredIndexes[Math.floor(Math.random() * filteredIndexes.length)]
+                        const randomCell = legalIndexes[Math.floor(Math.random() * legalIndexes.length)]
                         ghost.currentDirection = getDirection(randomCell, la)
                     } else {
-                        const preferedCellIndex = getShortestDistance(filteredIndexes)
+                        const preferedCellIndex = getShortestDistance(legalIndexes)
                         const directionOfIndex = getDirection(preferedCellIndex, la)
-                        // set direction to move in the cell with shortest distance
                         ghost.currentDirection = directionOfIndex
                     }
                 }
@@ -367,7 +277,7 @@ function ghostsMoves() {
         });
 
     }, gameSpeed * ghostSpeed)
-
+    arrOfIntervals.push(ghostInterval)
 }
 
 function pacmansMoves() {
@@ -375,20 +285,18 @@ function pacmansMoves() {
         const nextPreferredCell = getNextIndex(pacman.currentIndex, pacman.preferredDirection)
         const nextCell = getNextIndex(pacman.currentIndex, pacman.currentDirection)
         if (checkCellLegality(nextPreferredCell)) {
-            console.log(pacman.preferredDirection)
             moveCharacter(pacman, nextPreferredCell, pacman.preferredDirection)
             pacman.currentDirection = pacman.preferredDirection
 
         } else {
             if (checkCellLegality(nextCell)) {
-                console.log(pacman.preferredDirection)
-                console.log(pacman.currentDirection)
                 moveCharacter(pacman, nextCell, pacman.currentDirection)
             }
         }
         checkCell(pacman)
 
     }, gameSpeed)
+    arrOfIntervals.push(pacmanInterval)
 }
 
 function pacmanDirecton(event) {
@@ -438,8 +346,17 @@ function removeFrightenedMode() {
 function resetGame() {
     eatingSound.pause()
     eatingSound.currentTime = 0
-    clearInterval(pacmanInterval)
-    clearInterval(ghostInterval)
+    // todo fix the interval and timeout timing to attempt bug fix
+    console.log(arrOfIntervals)
+    for (const interval of arrOfIntervals) {
+        console.log('intnum', interval)
+        clearInterval(interval)
+        console.log('intind', arrOfIntervals.indexOf(interval))
+        // arrOfIntervals.splice(arrOfIntervals.indexOf(interval), 1)
+    }
+    console.log(arrOfIntervals)
+    // clearInterval(pacmanInterval)
+    // clearInterval(ghostInterval)
     clearTimeout(frightenedTime)
     removeFrightenedMode()
     arrOfGhosts.forEach(ghost => {
